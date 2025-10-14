45ec2b5a45e903de0f53744ae9c4529e624c4c8c
Nauthizful <nino.hamel@hotmail.com>
2025-10-14 14:26:23 +0200

    Création de la fonction moteurY; ajout de commentaires

--- CODE_CHARIOT/CODE_CHARIOT.ino
+++ CODE_CHARIOT/CODE_CHARIOT.ino
@@ -29,35 +29,26 @@ void setup()
 
   continu.attach(SERVO_CONTINU_PIN);
   moteur.attach(SERVO_MOTEUR_PIN);
-
-  Serial.begin(9600);
 }
 
 void loop()
 {
+  //Permet de limiter les valeurs renvoyées par le joystick à quelquechose d'utilisable pour le servo
   joystick_x = map(analogRead(JOYSTICK_X), 0, 1023, 75, 115);
-
-  // Si le joystick est dans un sens, on tourne le moteur au maximum dans une direction, sinon on le ferme
-  if(analogRead(JOYSTICK_Y) > 800){
-    joystick_y = MOTEUR_Y_MAX;
-  }
-  if(analogRead(JOYSTICK_Y) < 200){
-    joystick_y = MOTEUR_Y_MIN;
-  } 
-  
+ 
   //Pour chaque moteur, la valeur correspodante est appliquée
   continu.write(deadZone(joystick_x)); // Applique une zone morte sur l'axe X du joystick
-  moteur.write(joystick_y);
+  moteur.write(moteurY());
 }
 
-/*************************************************
+/********************************************************
 Permet d'appliquer une zone sur l'axe X du joystick
 pour éviter que le moteur ne bouge quand le joystick
 est au centre.
 
 Args:
   x (int): Valeur lue sur l'axe X du joystick
-**************************************************/
+*********************************************************/
 int deadZone(int x) 
 {
   if(x >= ANALOG_X_DEADZONE_MIN && x <= ANALOG_X_DEADZONE_MAX)
@@ -67,3 +58,17 @@ int deadZone(int x)
 
   return x;
 } 
+
+/********************************************************
+Si le joystick est dans un sens, on tourne le 
+moteur au maximum dans une direction, sinon dans l'autre.
+*********************************************************/
+int moteurY(){
+  // 
+  if(analogRead(JOYSTICK_Y) > 800){
+    return MOTEUR_Y_MAX;
+  }
+  if(analogRead(JOYSTICK_Y) < 200){
+    return MOTEUR_Y_MIN;
+  } 
+}
